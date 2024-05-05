1bf339bc506dc1b358e7eb33b81b5e08c73d2aa2
OXYGENKOLA <awwalolajide@gmail.com>
2024-05-05 18:23:40 +0200

    Added some features

--- lib/pages/mailbox.dart
+++ lib/pages/mailbox.dart
@@ -8,9 +8,7 @@ class Mailbox extends StatelessWidget {
     return  Scaffold(
       body: ListView.builder(
         itemCount: 10,
-       // Replace with actual email count
         itemBuilder: (context, index) {
-          // Build email entry here
           return const ListTile(
             leading: CircleAvatar(child: Text('A')), // Sender's icon/initial
             title: Text('Sender Name'), // Sender's name/email
--- lib/pages/meet.dart
+++ lib/pages/meet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Meet extends StatefulWidget {
   const Meet({super.key});
@@ -10,7 +11,63 @@ class Meet extends StatefulWidget {
 class _MeetState extends State<Meet> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        );
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: 40,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 60.0, top: 10),
+                  child: Text('New meeting', selectionColor: Colors.white),
+                ),
+              ),
+              Container(
+                height: 40,
+                width: 200,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 40.0, top: 10),
+                  child: Text('Join with a code', selectionColor: Colors.blue),
+                ),
+              )
+            ],
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                height: 590,
+                width: 500,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image(
+                      image: AssetImage("images/logo.jpg"),
+                      height: 200,
+                      width: 200,
+                      colorBlendMode: BlendMode.lighten,
+                    ),
+                    Text('Get a link you can share'),
+                    Text(
+                        'Tap New meeting to get a link you can share to people ')
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
